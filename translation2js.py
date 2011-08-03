636f557d149690aea6b61f52160a6a51706f9fc3
ltret <ltret@c7bacb06-31ca-45ca-81e0-dc0d7326716e>
2011-08-03 08:25:09 +0000

    ready to write {lang}.js
    git-svn-id: https://svn.bgdi.admin.ch/applications/translation/scripts@98 c7bacb06-31ca-45ca-81e0-dc0d7326716e

--- translation2js.py
+++ translation2js.py
@@ -2,6 +2,17 @@
 
 import os , sys
 
+# getting path for the input-file empty.js
+try:
+    if len(sys.argv) != 2:
+         print "You have to specify the Path to the directory i18n containing empty.js"
+         print "python translation2js.py <path to directory i18n>"
+         sys.exit()
+    Path2emptyjs = sys.argv[1]
+except:
+        sys.exit()
+
+
 try:
     import yaml
 except ImportError:
@@ -66,6 +77,59 @@ for lang in config['langs']:
 
     for row in rows:
         translationDict[lang][row["msg_id"]] = row[lang]
+
+
+# Parsing the file empty.js and write msgids into var_arr
+try:
+    file_emptyjs = open(Path2emptyjs + config["emptyFilename"],'r')
+except:
+    print "is the path to the directory i18n correct?"
+    sys.exit()
+var_arr = []
+for line in file_emptyjs:
+    int_begin = line.find("'") + 1
+    int_end = line.rfind("'")
+    if (int_begin > 0) and (int_end > 0):
+        var_arr.append(line[int_begin:int_end])
+
+
+# Writing the translated files
+for lang in config["langs"]:
+    print "Writing: " + lang + ".js"
+    print "------------------------"
+    try:
+        file_langjs = open(Path2emptyjs + lang + '.js','w')
+    except:
+        print "is the path to the directory i18n correct?"
+        sys.exit()
+
+    # Writing header
+    file_langjs.write("/*global OpenLayers:true*/\n\n/**\n * @requires OpenLayers/Lang/" + lang + ".js\n */\n\nOpenLayers.Util.extend(OpenLayers.Lang." + lang + ", {\n\n\t//Globals\n")
+
+    int_counter = 1
+    var_arr.sort()
+    for var_msgid in var_arr:
+        if int_counter < len(var_arr):
+            try:
+                print var_msgid + translationDict[lang][var_msgid]
+                file_langjs.write("\t'" + var_msgid + "':\"" + translationDict[lang][var_msgid].replace("'","`") + "\",\n")
+            except:
+                print var_msgid + " //TODO"
+                file_langjs.write("\t'" + var_msgid + "':\"" + var_msgid + "\", //TODO\n")
+
+        # the last entry and footer
+        else:
+            try:
+                print var_msgid + translationDict[lang][var_msgid]
+                file_langjs.write("\t'" + var_msgid + "':\"" + translationDict[lang][var_msgid].replace("'","`") + "\n});")
+            except:
+                print var_msgid + " //TODO"
+                file_langjs.write("\t'" + var_msgid + "':\"" + var_msgid + "\" //TODO\n});")
+
+
+        int_counter += 1
+
+    file_langjs.close()
 
 
 
