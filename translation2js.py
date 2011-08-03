ffd17f1b749390f78d66aa167c682a07d9e44656
ltmoc <ltmoc@c7bacb06-31ca-45ca-81e0-dc0d7326716e>
2011-08-03 13:58:19 +0000

    Fix unicode issues
    git-svn-id: https://svn.bgdi.admin.ch/applications/translation/scripts@103 c7bacb06-31ca-45ca-81e0-dc0d7326716e

--- translation2js.py
+++ translation2js.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os , sys
+import os, sys, codecs
 
 # getting path for the input-file empty.js
 try:
@@ -11,7 +11,6 @@ try:
     Path2emptyjs = sys.argv[1]
 except:
         sys.exit()
-
 
 try:
     import yaml
@@ -66,7 +65,7 @@ register_type(UNICODE)
 conn.set_client_encoding('UTF8')
 
 # Create a multinensional array [lang][msg-ud]  Example: translationDict["it"]["zoomin"]
-translationDict = {}
+translationDict = Ddict(dict)
 
 for lang in config['langs']:
 
@@ -76,12 +75,11 @@ for lang in config['langs']:
     rows = cur.fetchall()
 
     for row in rows:
-        translationDict[row["msg_id"]] = row[lang]
-
+        translationDict[lang][row["msg_id"]] = row[lang]
 
 # Parsing the file empty.js and write msgids into var_arr
 try:
-    file_emptyjs = open(Path2emptyjs + config["emptyFilename"],'r')
+    file_emptyjs = codecs.open(Path2emptyjs + config["emptyFilename"],'r', 'utf-8')
 except:
     print "is the path to the directory i18n correct?"
     sys.exit()
@@ -111,15 +109,14 @@ for lang in config["langs"]:
     var_arr.sort()
     for var_msgid in var_arr:
         try:
-            print var_msgid
-            myString = "\t'" + var_msgid + "':\'" + translationDict[var_msgid].replace("'","\\\'") + "\'"
+            myString = "\t'" + var_msgid + "':\'" + translationDict[lang][unicode(var_msgid)].replace("'","\\\'") + "\'"
             file_langjs.write(myString.encode('utf-8'))
             isTodo = ''
         except:
             file_langjs.write("\t'" + var_msgid + "':\'" + var_msgid + "\'")
             isTodo = ' //TODO'
 
-        if (int_counter < len(var_arr)):
+        if int_counter < len(var_arr):
             nextLine = ','
         else:
             nextLine = ''
